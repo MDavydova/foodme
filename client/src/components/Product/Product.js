10f1c0b72d9842ae234b121b6792c423d45f5f40
Milena Davydova <milena@yelk.com.ua>
2023-05-26 18:51:49 +0200

    products components

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -1,14 +1,27 @@
 import React from "react";
+import "./Product.scss";
 
-function Product({ image, description, price, name }) {
+function Product({ image, description, price, name, className }) {
   return (
-    <div>
+    <li className={className}>
       <div className="product__wrapper">
-        <strong>{name}</strong>
-        <img src={image} alt="image" />
-        <p>{description}</p>
+        <div className="flex align-center justify-between mb-32">
+          <strong className="product__title">{name}</strong>
+          <span className="product__price">${price}</span>
+        </div>
+        <div className="flex align-center justify-between mb-32">
+          <div className="product__image">
+            <img src={image} alt="image" />
+          </div>
+          <div className="product__buttons flex">
+            <button>+</button>
+            <button>-</button>
+          </div>
+        </div>
+
+        <p className="product__description">{description}</p>
       </div>
-    </div>
+    </li>
   );
 }
 
--- client/src/components/Shop/Shop.js
+++ client/src/components/Shop/Shop.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Product from "../Product/Product";
 import "./Shop.scss";
 
 function Shop({ shopName, shopLocation, range, className }) {
+  const productsList = range.map((product, index) => (
+    <Product key={index} {...product} className="shop__products-item" />
+  ));
   return (
     <li className={className}>
       <div className="shop__wrapper">
         <strong className="shop__name">{shopName}</strong>
+
+        <ul className="shop__products-list">{productsList}</ul>
       </div>
     </li>
   );

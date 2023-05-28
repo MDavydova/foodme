3ad4d3611245d283bcfc9342bcbb6a219e3fed8b
Milena Davydova <milena@yelk.com.ua>
2023-05-28 12:49:20 +0200

    added markup for form

--- client/src/components/CheckoutForm/CheckoutForm.js
+++ client/src/components/CheckoutForm/CheckoutForm.js
@@ -1,73 +1,220 @@
 import "./CheckoutForm.scss";
-import { Formik } from "formik";
 import React from "react";
+import * as yup from "yup";
+import "yup-phone";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  FormFeedback,
+  Input,
+  InputGroup,
+  InputGroupText,
+  Col,
+  Label,
+  Form,
+  FormGroup,
+} from "reactstrap";
+
+const phoneSchema = yup.string().phone().required("Phone is required");
+const nameSchema = yup
+  .string()
+  .min(2, "Too Short!")
+  .max(50, "Too Long!")
+  .required("Name is required");
+const emailSchema = yup
+  .string()
+  .email("Invalid email")
+  .required("Email is required");
+const addressSchema = yup
+  .string()
+  .min(2, "Too Short!")
+  .max(50, "Too Long!")
+  .required("Address is required");
 
 function CheckoutForm() {
+  const [phone, setPhone] = useState("");
+  const [phoneValid, setPhoneValid] = useState("");
+  const [name, setName] = useState("");
+  const [nameValid, setNameValid] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailValid, setEmailValid] = useState("");
+  const [address, setAddress] = useState("");
+  const [addressValid, setAddressValid] = useState("");
+
+  const validatePhone = async (phone) => {
+    const isValid = await phoneSchema.isValid(phone);
+    setPhoneValid(isValid);
+  };
+
+  const validateName = async (phone) => {
+    const isValid = await nameSchema.isValid(name);
+    setNameValid(isValid);
+  };
+
+  const validateEmail = async (phone) => {
+    const isValid = await emailSchema.isValid(email);
+    setEmailValid(isValid);
+  };
+  const validateAddress = async (phone) => {
+    const isValid = await addressSchema.isValid(address);
+    setAddressValid(isValid);
+  };
+
+  useEffect(() => {
+    validatePhone(phone);
+    validateName(name);
+    validateEmail(email);
+    validateAddress(address);
+  }, [phone, name, email, address]);
+
+  const onChangePhoneHandler = (ev) => {
+    const { value } = ev.target;
+    setPhone(value.trim());
+  };
+  const onChangeNameHandler = (ev) => {
+    const { value } = ev.target;
+    setName(value.trim());
+  };
+  const onChangeEmailHandler = (ev) => {
+    const { value } = ev.target;
+    setEmail(value.trim());
+  };
+  const onChangeAddressHandler = (ev) => {
+    const { value } = ev.target;
+    setAddress(value.trim());
+  };
+
+  const resetHandler = () => {
+    setPhone("");
+  };
+
+  const resetNameHandler = () => {
+    setName("");
+  };
+  const resetEmailHandler = () => {
+    setEmail("");
+  };
+  const resetAddressHandler = () => {
+    setAddress("");
+  };
+
+  const formSubmitHandler = (ev) => {
+    ev.preventDefault();
+  };
+
   return (
     <div>
-      <Formik
-        initialValues={{ email: "", name: "" }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = "Invalid email address";
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-        }) => (
-          <form onSubmit={handleSubmit} className="flex flex-col">
-            <input
-              type="email"
-              name="email"
-              placeholder="your email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            {errors.email && touched.email && errors.email}
-            <input
-              type="text"
-              name="name"
-              placeholder="your name"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.name}
-            />
-            {errors.name && touched.name && errors.name}
-            <input
-              type="tel"
-              name="phone"
-              placeholder="your phone"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.phone}
-            />
-            {errors.phone && touched.phone && errors.phone}
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </form>
-        )}
-      </Formik>
+      <Form onSubmit={formSubmitHandler}>
+        <FormGroup row>
+          <Col>
+            <InputGroup className="mb-24">
+              <InputGroupText>
+                <Label for="phone">
+                  <span role="img" aria-label="phone emoji">
+                    ☎️
+                  </span>
+                </Label>
+              </InputGroupText>
+              <Input
+                id="phone"
+                name="phone"
+                placeholder="+38099999999"
+                type="tel"
+                valid={phoneValid}
+                invalid={!phoneValid}
+                onChange={onChangePhoneHandler}
+                value={phone}
+              />
+              <Button type="button" onClick={resetHandler}>
+                <span role="img" aria-label="reset phone input">
+                  🔄
+                </span>
+              </Button>
+              <FormFeedback valid>You've entered a phone.</FormFeedback>
+              <FormFeedback invalid>
+                <em>You've entered an invalid phone.</em>
+              </FormFeedback>
+            </InputGroup>
+            <InputGroup className="mb-24">
+              <InputGroupText>
+                <Label for="name">
+                  <span>Name</span>
+                </Label>
+              </InputGroupText>
+              <Input
+                id="name"
+                name="name"
+                placeholder="Your name"
+                type="name"
+                valid={nameValid}
+                invalid={!nameValid}
+                onChange={onChangeNameHandler}
+                value={name}
+              />
+              <Button type="button" onClick={resetNameHandler}>
+                <span role="img" aria-label="reset name input">
+                  🔄
+                </span>
+              </Button>
+              <FormFeedback valid>You've entered a valid name.</FormFeedback>
+              <FormFeedback invalid>
+                <em>You've entered an invalid name.</em>
+              </FormFeedback>
+            </InputGroup>
+            <InputGroup className="mb-24">
+              <InputGroupText>
+                <Label for="email">
+                  <span>Email</span>
+                </Label>
+              </InputGroupText>
+              <Input
+                id="email"
+                name="email"
+                placeholder="email@gmail.com"
+                type="email"
+                valid={emailValid}
+                invalid={!emailValid}
+                onChange={onChangeEmailHandler}
+                value={email}
+              />
+              <Button type="button" onClick={resetEmailHandler}>
+                <span role="img" aria-label="reset email input">
+                  🔄
+                </span>
+              </Button>
+              <FormFeedback valid>You've entered a valid email.</FormFeedback>
+              <FormFeedback invalid>
+                <em>You've entered an invalid email.</em>
+              </FormFeedback>
+            </InputGroup>
+            <InputGroup>
+              <InputGroupText>
+                <Label for="address">
+                  <span>Address</span>
+                </Label>
+              </InputGroupText>
+              <Input
+                id="address"
+                name="address"
+                placeholder="Your address"
+                valid={addressValid}
+                invalid={!addressValid}
+                onChange={onChangeAddressHandler}
+                value={address}
+              />
+              <Button type="button" onClick={resetAddressHandler}>
+                <span role="img" aria-label="reset address input">
+                  🔄
+                </span>
+              </Button>
+              <FormFeedback valid>You've entered a valid address.</FormFeedback>
+              <FormFeedback invalid>
+                <em>You've entered an invalid address.</em>
+              </FormFeedback>
+            </InputGroup>
+          </Col>
+        </FormGroup>
+      </Form>
     </div>
   );
 }

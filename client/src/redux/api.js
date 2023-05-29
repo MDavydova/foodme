28e8991e06338b4335a4732ac696e051ffbc7542
Milena Davydova <milena@yelk.com.ua>
2023-05-29 21:35:59 +0200

    fixes

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? window.API_URL
+    ? process.env.REACT_APP_MY_ENV
     : process.env.REACT_APP_API_URL;
 
 const API = axios.create({ baseURL: API_URL });

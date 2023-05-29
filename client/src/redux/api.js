244469e657a04fd393c0815637fd46a2b3d09bc6
Milena Davydova <milena@yelk.com.ua>
2023-05-29 22:14:14 +0200

    fixes

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -10,4 +10,5 @@ const API = axios.create({ baseURL: API_URL });
 export const createOrder = (orderData) => API.post("/order", orderData);
 export const deleteOrder = (id) => API.delete(`/order/${id}`);
 export const getOrderByKey = (data) => API.post("/order/getOrderByKey", data);
-export const getShops = () => API.get("/shops");
+export const getShops = () =>
+  axios.get("https://foodme-m75s.onrender.com/shops");

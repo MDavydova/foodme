9bc7c15c3a171cde05deb72acc31d9c47eeee263
Milena Davydova <milena@yelk.com.ua>
2023-05-29 20:50:10 +0200

    fixes

--- server/client/src/redux/api.js
+++ server/client/src/redux/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
-
-export const createOrder = (orderData) => API.post("/order", orderData);
-export const deleteOrder = (id) => API.delete(`/order/${id}`);
-export const getOrderByKey = (data) => API.post("/order/getOrderByKey", data);
-export const getShops = () => API.get("/shops");
+export const createOrder = (orderData) => axios.post("/order", orderData);
+export const deleteOrder = (id) => axios.delete(`/order/${id}`);
+export const getOrderByKey = (data) => axios.post("/order/getOrderByKey", data);
+export const getShops = () => axios.get("/shops");

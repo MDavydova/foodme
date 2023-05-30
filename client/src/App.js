9ee204c5a31627de32d883dcb4749e13c7f156bc
Milena Davydova <milena@yelk.com.ua>
2023-05-30 15:38:05 +0200

    added spinner

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,9 @@ import Navbar from "./components/Navbar/Navbar";
 import CartPage from "./pages/CartPage/CartPage";
 import OrderInfoPage from "./pages/OrderInfoPage/OrderInfoPage";
 import ShopsPage from "./pages/ShopsPage/ShopsPage";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const location = useLocation();
   return (
     <>
       <Navbar />
--- client/src/pages/ShopsPage/ShopsPage.js
+++ client/src/pages/ShopsPage/ShopsPage.js
@@ -1,15 +1,21 @@
 import React, { useEffect } from "react";
 import "../ShopsPage/ShopsPage.scss";
 import { useDispatch, useSelector } from "react-redux";
+import ClipLoader from "react-spinners/ClipLoader";
 import { getShops } from "../../redux/features/shopsSlice";
 import Shop from "../../components/Shop/Shop";
 
 function ShopsPage({ pageClassName }) {
-  const { shops } = useSelector((state) => ({
+  const { shops, loading } = useSelector((state) => ({
     ...state.shops,
   }));
 
   const dispatch = useDispatch();
+
+  const styles = {
+    margin: "0 auto",
+    display: "block",
+  };
 
   useEffect(() => {
     dispatch(getShops());
@@ -23,6 +29,13 @@ function ShopsPage({ pageClassName }) {
     <main className={pageClassName}>
       <div className="container">
         <h1>Choose the shop</h1>
+        <ClipLoader
+          loading={loading}
+          size={150}
+          cssOverride={styles}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
         <ul className="shops__list">{shopsList}</ul>
       </div>
     </main>
